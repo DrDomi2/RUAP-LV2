9e4c50f1c0efb06e6b6e4e465b84fcd75e359c9a
DrDomi2 <dvidovic1@etfos.hr>
2023-11-06 12:55:54 +0100

    Read only API

---
+++ ContactManager/ContactManager.Tests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using ContactManager;
+using ContactManager.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Web.Mvc;
+
+namespace ContactManager.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            // Arrange
+            HomeController controller = new HomeController();
+
+            // Act
+            ViewResult result = controller.Index() as ViewResult;
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual("Home Page", result.ViewBag.Title);
+        }
+    }
+}
---
+++ ContactManager/ContactManager/Controllers/ContactController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using ContactManager.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ContactManager.Models.Services;
+
+namespace ContactManager.Controllers
+{
+    public class ContactController : ApiController
+    {
+        private ContactRepository contactRepository;
+        public ContactController()
+        {
+            this.contactRepository = new ContactRepository();
+        }
+
+        public Contact[] Get()
+        {
+            return contactRepository.GetAllContacts();
+        }
+    }
+}
